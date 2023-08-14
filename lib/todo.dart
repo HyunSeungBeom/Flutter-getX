d7712223616a5165c7b1aec85a9528c01afb0c31
현승범 <ghfk100@naver.com>
2023-08-14 20:25:38 +0900

    getX를 이용한 todo 앱 탄생

---
+++ lib/todo.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class Task {
+  final String title;
+  late final bool isDone;
+
+  Task(this.title, this.isDone);
+}
+
+class TaskController extends GetxController {
+  var tasks = <Task>[].obs;
+
+  void addTask(String title) {
+    tasks.add(Task(title, false));
+  }
+
+  void toggleTask(int index) {
+    tasks[index].isDone = !tasks[index].isDone;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  final taskController = Get.put(TaskController());
+
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Todo App')),
+        body: Column(
+          children: [
+            Expanded(child: TaskList()),
+            TaskInput(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  final taskController = Get.find<TaskController>();
+
+  TaskList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: taskController.tasks.length,
+      itemBuilder: (context, index) {
+        final task = taskController.tasks[index];
+        return ListTile(
+          title: Text(task.title),
+          trailing: Checkbox(
+            value: task.isDone,
+            onChanged: (value) => taskController.toggleTask(index),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class TaskInput extends StatelessWidget {
+  final taskController = Get.find<TaskController>();
+  final TextEditingController textEditingController = TextEditingController();
+
+  TaskInput({super.key});
+
+  void addTask() {
+    final title = textEditingController.text;
+    if (title.isNotEmpty) {
+      taskController.addTask(title);
+      textEditingController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: textEditingController,
+              decoration: const InputDecoration(labelText: 'Add a new task'),
+              onSubmitted: (_) => addTask(),
+            ),
+          ),
+          const SizedBox(width: 10),
+          ElevatedButton(
+            onPressed: addTask,
+            child: const Text('Add'),
+          ),
+        ],
+      ),
+    );
+  }
+}
